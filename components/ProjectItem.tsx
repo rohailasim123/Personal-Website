8d4cacab09a4bb0b61dd8e609e4ab9d3d49bb47e
Rohail Asim <ra3106@nyu.edu>
2025-07-20 15:17:34 -0400

    nit

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -5,7 +5,7 @@ interface ProjectProps {
 
 const ProjectItem = ({project, index}: ProjectProps): JSX.Element => {
     return (
-      <a href={project.href} target={index == 6 ? "_blank" : ""}>
+      <a href={project.href} target={index == 5 ? "_blank" : ""}>
         <div className="shadow-lg mb-8 mx-auto lg:w-14/15 lg:flex lg:flex-row lg:h-auto">
           <img className="rounded-tr-lg rounded-tl-lg h-68 w-full lg:h-auto lg:w-6/12 lg:rounded-bl-lg lg:rounded-tr-none" src={project.img} alt="" />
           <div className="w-full bg-gray-50 p-8 rounded-bl-lg rounded-br-lg lg:rounded-bl-none lg:rounded-tr-lg">
